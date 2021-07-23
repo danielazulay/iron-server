7f1291d32780d93b1f1dc2f8e99562231b98bdaa
Daniel Azulay <you@example.comdanielazulay20@gmail.com>
2021-07-23 16:54:57 -0300

    router/product:delete/ edit

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -18,4 +18,37 @@ return res.status(200).json(resposta)
     }
 })
 
+router.put("/editProduct/:id",isAuthenticated,async(req,res,next)=>{
+try{
+
+    const data= req.body
+    const {id} = req.params
+
+const resposta = await productModule.findByIdAndUpdate({_id:id},{...data},{ new: true })
+return res.status(200).json(resposta)
+}catch(err){
+    next(err)
+}
+
+})
+
+router.delete("/deleteProduct/:id",isAuthenticated,async(req,res,next)=>{
+
+try{
+    const {id} = req.params
+
+const resposta = await productModule.findOneAndDelete({_id:id})
+
+if(resposta.n<1){
+
+ return res.status(404).json({ error: "produto nao existe" });
+}
+
+return res.status(200).json({})
+} catch (err) {
+    next(err);
+  }
+}
+);
+
 module.exports = router;
