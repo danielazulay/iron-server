74fb8318d7c888db9f65f74b6cd272e058bdfac5
GianLucas <gianspf@gmail.com>
2021-07-25 19:02:43 +0200

    Enter

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,79 +1,104 @@
 const router = require("express").Router();
 
 const isAuthenticated = require("../middlewares/isAuthenticated");
-const attachCurrentUser = require("../middlewares/attachCurrentUser")
+const attachCurrentUser = require("../middlewares/attachCurrentUser");
 
-const productModule = require("../models/Product.model")
+const productModule = require("../models/Product.model");
 
-const orderModule = require("../models/Order.model")
-const isAdmin = require("../middlewares/isAdmin")
+const orderModule = require("../models/Order.model");
+const isAdmin = require("../middlewares/isAdmin");
 
+router.post(
+  "/newOrder",
+  isAuthenticated,
+  attachCurrentUser,
+  async (req, res, next) => {
+    // Extrair o id do projeto dos parâmetros de rota
+    try {
+      const result = await orderModule.create({
+        userid: req.currentUser._id,
+      });
 
-router.post("/newOrder",isAuthenticated,attachCurrentUser,async(req,res,next)=>{
-
-
-  
-        // Extrair o id do projeto dos parâmetros de rota
-        try {
-            const result = await orderModule.create({
-              
-                userid: req.currentUser._id
-            });
-      
-            return res.status(201).json(result);
-          } catch (err) {
-            next(err);
-          }
-
-
-})
-
-
-router.get("/userOrders",isAuthenticated,attachCurrentUser,async(req,res,next)=>{
-
-    try{
-
-        
-const result = await orderModule.find({userid:req.currentUser._id}).populate("Product")
-
-return res.status(200).json(result);
-
-    }catch(err){next(err)}
-})
-
-
-
-
-router.get("/allOrders",isAuthenticated,attachCurrentUser,isAdmin,async(req,res,next)=>{
-
-    try{
-
-        
-const result = await orderModule.find().populate("Product")
-
-return res.status(200).json(result);
-
-    }catch(err){next(err)}
-})
-
-
-router.put("/insertPorducts/:id",isAuthenticated,attachCurrentUser,async(req,res,next)=>{
-
-    try{
-
-        
-const result = await orderModule.findOneAndUpdate({_id:req.params.id},{$push:{ ...req.body}})
-
-const productnew = await productModule.findOneAndUpdate({_id:result.productid},{$push:{userid:req.currentUser._id} ,$inc:{unity:-1} })
-
- 
-if (result) {
-    return res.status(200).json(result);
+      return res.status(201).json(result);
+    } catch (err) {
+      next(err);
+    }
   }
+);
 
-  return res.status(404).json({ error: "Projeto não encontrado." });
+router.get(
+  "/userOrders",
+  isAuthenticated,
+  attachCurrentUser,
+  async (req, res, next) => {
+    try {
+      const result = await orderModule
+        .find({ userid: req.currentUser._id })
+        .populate("Product");
 
-    }catch(err){next(err)}
+      return res.status(200).json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get(
+  "/allOrders",
+  isAuthenticated,
+  attachCurrentUser,
+  async (req, res, next) => {
+    try {
+      const result = await orderModule.find().populate("Product");
+
+      return res.status(200).json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  "/insertPorducts/:id",
+  isAuthenticated,
+  attachCurrentUser,
+  async (req, res, next) => {
+    try {
+      const result = await orderModule.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { ...req.body } },
+        {new : true}
+      );
+
+      const productnew = await productModule.findOneAndUpdate(
+        { _id: result.productid },
+        { $push: { userid: req.currentUser._id }, $inc: { unity: -1 }},
+        {new : true}
+      );
+
+      if (result) {
+        return res.status(200).json(result);
+      }
+
+      return res.status(404).json({ error: "Projeto não encontrado." });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete("/deleteOrder/:id", isAuthenticated,attachCurrentUser, async (req, res, next) => {
+  try{
+
+      const deleteOrder = await orderModule.deleteOne({ _id: req.params.id })
+
+      if(deleteOrder.n > 0){
+          return res.status(200).json({})
+      }
+      return res.status(404).json({ error: "Projeto não encontrado." });
+  }catch(err){
+    next(err)
+  }
 })
 
 module.exports = router;
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -7,7 +7,7 @@ const isAuthenticated = require("../middlewares/isAuthenticated");
 
 const productModule = require("../models/Product.model")
 
-router.post("/newProduct",isAuthenticated,isAdmin,async(req,res,next)=>{
+router.post("/newProduct",isAuthenticated,async(req,res,next)=>{
     try{
         const data= req.body
 
@@ -20,7 +20,7 @@ return res.status(200).json(resposta)
     }
 })
 
-router.put("/editProduct/:id",isAuthenticated,isAdmin,async(req,res,next)=>{
+router.put("/editProduct/:id",isAuthenticated,async(req,res,next)=>{
 try{
 
     const data= req.body
@@ -34,7 +34,7 @@ return res.status(200).json(resposta)
 
 })
 
-router.delete("/deleteProduct/:id",isAuthenticated,isAdmin,async(req,res,next)=>{
+router.delete("/deleteProduct/:id",isAuthenticated,async(req,res,next)=>{
 
 try{
     const {id} = req.params
