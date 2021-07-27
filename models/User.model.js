50f9bfa2daeffd1c5461772219d9de282306c926
GianLucas <gianspf@gmail.com>
2021-07-27 23:22:50 +0200

    GianLucas - UserModel, User.server && Product.router

--- models/User.model.js
+++ models/User.model.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true, trim: true },
   passwordHash: { type: String, required: true },
   document: { type: String, required: true },
-  eddress: new mongoose.Schema({
+  address: new mongoose.Schema({
     street: { type: String, required: true, trim: true },
     neighbourhood: { type: String, required: true, trim: true },
     city: { type: String, required: true, trim: true },
