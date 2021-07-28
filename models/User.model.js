b0ca6528f0a7123816e69750aefba9ea4eef88ec
GianLucas <gianspf@gmail.com>
2021-07-28 15:29:57 +0200

    GianLucas - User.Model

--- models/User.model.js
+++ models/User.model.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema({
     street: { type: String, required: true, trim: true },
     neighbourhood: { type: String, required: true, trim: true },
     district: { type: String, required: true, trim: true },
-    state: { type: String, required: true, trim: true },
+    city: { type: String, required: true, trim: true },
     postalCode: { type: String, required: true, trim: true },
     number: { type: String, required: true, trim: true },
   }),
