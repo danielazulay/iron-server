7919deaa3804807fd409b5e3ec43bc2a7900e272
Daniel Azulay <you@example.comdanielazulay20@gmail.com>
2021-08-09 11:59:44 -0300

    api

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 
 
-const cors = require("cors");
 require("dotenv").config();
+
 const express = require("express");
 
+const cors = require("cors");
+
+
+
 const connectToDb = require("./config/db.config");
+
+
 const userRouter = require("./routes/user.routes");
 const productRouter = require("./routes/product.routes");
 const orderRouter  = require("./routes/order.routes")
-var proxy = require('http-proxy-middleware')
+
 
 
 
 const app = express();
+
 app.use(cors({ origin: "*" }));  
 /* 
 app.use(function(req,res,next){
@@ -29,13 +36,7 @@ async function init() {
 
   const db = await connectToDb();
 
-    app.use(express.json());
-
-
-    console.log("Conectado ao banco de dados!");
-
-    app.use("/", userRouter);
-
+  app.use(express.json());
 
 console.log("Conectado ao banco de dados!");
 
@@ -45,7 +46,9 @@ app.use("/api", productRouter);
 
 app.use("/api", orderRouter);
 
-    app.listen(4000, () => console.log("Servidor rodando na porta 4000!"));
+app.listen(Number(process.env.PORT), () =>
+console.log(`Server up and running at port ${process.env.PORT}`)
+);
   } catch (err) {
     console.log("Erro ao conectar ao banco de dados!", err);
     process.exit(1);
