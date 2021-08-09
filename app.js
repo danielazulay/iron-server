5f64c2158abc488ac0a90076e29b4377ee3d1035
Daniel Azulay <you@example.comdanielazulay20@gmail.com>
2021-08-09 12:09:44 -0300

    api

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const express = require("express");
 
 const cors = require("cors");
 
+require(".//config/db.config")();
 
+const app = express();
 
-const connectToDb = require("./config/db.config");
+app.use(express.json());
+
+app.use(cors({ origin: "*" }));  
+
+/* const connectToDb = require("./config/db.config"); */
 
 
 const userRouter = require("./routes/user.routes");
@@ -18,9 +24,8 @@ const orderRouter  = require("./routes/order.routes")
 
 
 
-const app = express();
 
-app.use(cors({ origin: "*" }));  
+
 /* 
 app.use(function(req,res,next){
   res.header("Acess-Control-Allow-Origin","*");
@@ -29,16 +34,16 @@ app.use(function(req,res,next){
 }) */
 
 
-async function init() {
-  try {
+/* async function init() {
+  try { */
  
 
 
-  const db = await connectToDb();
+/*   const db = await connectToDb();
 
-  app.use(express.json());
 
-console.log("Conectado ao banco de dados!");
+
+console.log("Conectado ao banco de dados!"); */
 
 app.use("/api", userRouter);
 
@@ -49,9 +54,10 @@ app.use("/api", orderRouter);
 app.listen(Number(process.env.PORT), () =>
 console.log(`Server up and running at port ${process.env.PORT}`)
 );
-  } catch (err) {
+  /* } catch (err) {
     console.log("Erro ao conectar ao banco de dados!", err);
     process.exit(1);
-  }
-}
+  } */
+/* }
 init();
+ */
