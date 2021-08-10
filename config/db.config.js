f7f1f8043bbc763ab46d150d6c72fc4a41e11822
Daniel Azulay <you@example.comdanielazulay20@gmail.com>
2021-08-10 10:38:25 -0300

    a

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,19 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose')
-
-function connectToDB() {
-    return mongoose.connect(process.env.MONGODB_URI, {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-       
-    })
+async function connect() {
+  try {
+    // Não esquecer de criar variável de ambiente com endereço do seu servidor Mongo local em desenvolvimento, e o endereço do cluster do Atlas em produção
+    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log("Connected to DB: ", connection.connection.name);
+  } catch (err) {
+    console.error("Database connection error: ", err);
+  }
 }
+
 
 module.exports = connectToDB
